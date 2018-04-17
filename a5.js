48ba2dd78bd4f64efb23a77778ac3fb5382d49c4
howardluong <37283238+howardluong@users.noreply.github.com>
2018-04-16 20:55:10 -0400

    Add files via upload

---
+++ a5.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+
+})
+
+
+function openMorePerformance() {
+	document.getElementById('morePerformanceContent').style.display = 'block';
+}
+
+function closeMorePerformance() {
+	document.getElementById('morePerformanceContent').style.display = 'none';
+}
+
+function openOuterAspectsModal() {
+	document.getElementById('outerAspectsModal').style.display = 'block';
+}
+
+function closeOuterAspectsModal() {
+	document.getElementById('outerAspectsModal').style.display = 'none';
+}
+
+function openSideModal() {
+  document.getElementById('sideModal').style.display = "block";
+}
+
+function openTopModal() {
+	document.getElementById('topModal').style.display = 'block'
+}
+
+function openFrontBackModal() {
+	document.getElementById('frontBackModal').style.display = 'block';
+}
+
+function closeSideModal() {
+  document.getElementById('sideModal').style.display = 'none';
+}
+
+function closeTopModal() {
+	document.getElementById('topModal').style.display = 'none';
+}
+
+function closeFrontBackModal() {
+	document.getElementById('frontBackModal').style.display = 'none';
+}
+
+function openInnerBeautyText() {
+	document.getElementById('innerBeautyTextModal').style.display = 'block';
+}
+
+function closeInnerBeautyText() {
+	document.getElementById('innerBeautyTextModal').style.display = 'none';
+}
