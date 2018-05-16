eec895983b0fe79265e78ff86786502d2ebb1a20
howardluong <37283238+howardluong@users.noreply.github.com>
2018-05-15 23:20:50 -0400

    Add files via upload

--- a5.js
+++ a5.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
 
+	var $animation_elements = $('.animation-element');
+	var $window = $(window);
+
+	$window.on('scroll resize', check_if_in_view);
+	$window.trigger('scroll');
+
+	function check_if_in_view(){
+		var window_height = $window.height();
+		var window_top_position = $window.scrollTop();
+		var window_bottom_position = (window_height + window_top_position);
+
+		$.each($animation_elements, function(){
+			var $element = $(this);
+			var element_height = $element.outerHeight();
+			var element_top_position = $element.offset().top;
+			var element_bottom_position = (element_height + element_top_position);
+
+			if((element_bottom_position >= window_top_position) &&
+				(element_top_position <= window_bottom_position)) {
+				$element.addClass('in-view');
+			} else {
+				$element.removeClass('in-view');
+			}
+		})
+	}
+
 });
 
 var a5Brand = new Vue({
@@ -31,6 +57,7 @@ function showPage() {
   document.getElementById("preload").style.opacity = 0;
   document.getElementById("mainContent").style.opacity = 1;
   document.getElementById("preload").style.zIndex = 0;
+  document.getElementById("mainContent").style.overflowY = 'scroll';
 }
 
 function openMorePerformance() {
